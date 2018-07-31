720b89763a7bcea3f9911ef98b8f38041b23f15e
kplaminack <40872769+kplaminack@users.noreply.github.com>
2018-07-31 13:41:25 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,80 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = matrix()) {     	#creates a matrix
+        i <- NULL                               	#sets inverse object to be used later in the code
+        set <- function(y) {				
+                x <<- y						#assigns the input argument to the x object in the parent environment
+                i <<- NULL					#clears any value of i that had been cached by a prior execution of cacheSolve()
+        }
+        get <- function() x					#defines getter for matrix x
+        setinverse <- function(inverse) i <<- inverse	#assigns the input argument to the value of i in the parent environment
+        getinverse <- function() i				#defines getter for inverse i
+        list(set = set, get = get,				#assigns each of these functions as an element within a list and returns it to the parent environment
+             setinverse = setinverse,
+             getinverse = getinverse)
+}
 
-makeCacheMatrix <- function(x = matrix()) {
-
+cacheSolve <- function(x, ...) {				
+        i <- x$getinverse()					#retrieves the inverse from makeCacheMatrix
+        if(!is.null(m)) {					#if value is null, matrix is new; else, it's inverse is cached
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()						#gets the matrix from the input object
+        i <- solve(data, ...)					#calculates inverse
+        x$setinverse(i)						#assigns inverse
+        i								#prints inverse
 }
 
 
-## Write a short comment describing this function
+m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+m1
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+I2 <- matrix(c(1,0,0,1), nrow = 2, ncol = 2)
+I2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
